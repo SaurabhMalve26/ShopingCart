62df8b3d97eaaa87107c5bc25af95b8c483e7a1d
SaurabhMalve56 <saurabh.malve@consociatesolutions.com>
2025-10-18 11:51:01 +0530

    created about us poage

---
+++ app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page: React.FC = () => {
+  return (
+    <div className="text-5xl flex justify-center items-center relative h-screen ">
+        <p className=" absolute   ">
+      aboutus 
+        </p>
+    </div>
+  )
+}
+export default page
